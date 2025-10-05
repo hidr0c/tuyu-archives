1f0ce6282e0df46c59728ad8cfe0853061892b4d
ThaiPX <phanxuanthai2004@gmail.com>
2025-10-05 09:55:35 +0700

    Update asset paths in configuration and styles for consistency

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: "/tuyu-archives",
   output: "standalone",
   typescript: {
     ignoreBuildErrors: true,
